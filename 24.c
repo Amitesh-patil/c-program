1e6bf844f297357d82ae159b7057e4b79a09ef0a
Amitesh-patil <47466513+Amitesh-patil@users.noreply.github.com>
2019-02-11 06:29:12 +0530

    Create 24.c

---
+++ 24.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+ int i,j,n,num[10],a;
+ scanf("%d",&n);
+ for(i=0;i<n;++i)
+ {
+  scanf("%d",&num[i]);
+ }
+ for(i=0;i<n;++i)
+ {
+  for(j=i+1;j<n;++j)
+   {
+     if(num[i]>num[j])
+       {
+         a=num[i];
+         num[i]=num[j];
+         num[j]=a;
+        }
+    }
+ }
+ for(i=0;i<n;++i)
+ {
+ printf("%d",num[i]);
+ }
+return 0;
+}
